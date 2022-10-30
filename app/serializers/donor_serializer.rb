eae02e3265018111bbe9b60a2204da59101c69fa
Njeri Ngungu <janetnjerin89@gmail.com>
2022-10-30 14:44:30 +0300

    done

--- app/serializers/donor_serializer.rb
+++ app/serializers/donor_serializer.rb
@@ -1,4 +1,4 @@
 class DonorSerializer < ActiveModel::Serializer
-  attributes :first_name, :last_name, :email, :reminder, :donate, :mode_of_donation, :status, :frequecy
+  attributes :first_name, :last_name, :email, :reminder, :donate, :mode_of_donation, :status, :frequency
    
 end
